1e9d452ac604a2c1b8f3875b7e3a737ae55ba784
Bluemorpho61 <vanda10.dava@gmail.com>
2022-11-15 07:26:33 +0700

    - Penambahan session pada sistem login - File baru: logout.php  15/11/2022 07:26

--- Controller/cek_login.php
+++ Controller/cek_login.php
@@ -1,48 +1,36 @@
 <?php
-include('../Config/koneksi.php');
+$koneksi =mysqli_connect("localhost", "root", "", "db_warungkuy");
 session_start();
-if (isset($_POST['submit'])) {
-    $email = $_POST['email-log'];
-    $pass = $_POST['pass-log'];
+if (isset($_POST['submit'])){
+    $email=$_POST['email-log'];
+    $pass=$_POST['pass-log'];
 
-    if (!empty(trim($email)) && !empty(trim($pass))) {
-        $query = "SELECT * FROM tb_users WHERE email='$email'";
-        $result = mysqli_query($koneksi, $query);
-        $num = mysqli_num_rows($result);
-        while ($row = mysqli_fetch_array($result)) {
-            $id = $row['id_user'];
-            $username = $row['username'];
-            $userEmail = $row['email'];
-            $userPass = $row['password'];
-            $hak = $row['hak'];
+    if (!empty(trim($email))&& !empty(trim($pass))){
+        $query="SELECT * FROM tb_users WHERE email ='$email'";
+        $result=mysqli_query($koneksi,$query);
+        $num =mysqli_num_rows($result);
+        while ($row =mysqli_fetch_array($result)){
+            $id =$row['id_user'];
+            $userVal=$row['email'];
+            $pasVal=$row['password'];
+            $level=$row['hak'];
         }
-        if ($num != 0) {
-            if ($userEmail == $email && $userPass == $pass) {
-                if ($hak == 'admin') {
-                    ?>
-                    <script>alert("Anda masuk sebagai admin")</script>
-                    <?php
-                    header("Location:../View/index.php?username=" . urlencode($username));
-                } elseif ($hak == 'registered') {
-                    echo 'Selamat datang ' . $username;
-                }
-            } else {
-                ?>
-                <script>alert("Username / Password salah")</script><?php
-                header('Location:../View/LoginPage.html');
+        if ($num !=0){
+            if ($userVal==$email &&$pasVal==$pass&&$level=='admin'){
+                //  header('Location: home.php?user_fullname='. urldecode($userName));
+                $_SESSION['id_user']=$id;
+                $_SESSION['email']=$userVal;
+                $_SESSION['password']=$pasVal;
+                $_SESSION['hak']=$level;
+                header('Location: ../View/index.php');
+            }else{
+                $error='user atau password salah!!';
+                header('Location: ../View/LoginPage.html');
             }
-        } else {
-            ?>
-            <script>alert("User tidak ditemukan / belum terdaftar")</script><?php
-            header('Location:../View/LoginPage.html');
+        }else{
+            $error='user tidak ditemukan';
+            echo $error;
         }
-    } else {
-        ?>
-        <script>alert("Field jangan sampai kosong")</script><?php
-        header('Location:../View/LoginPage.html');
     }
+
 }
-
-
-
-
