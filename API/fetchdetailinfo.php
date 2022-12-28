9ed588c184e980ea98a74c92815d2c5fcc6803d6
Bluemorpho61 <vanda10.dava@gmail.com>
2022-12-29 06:46:43 +0700

    Menambahkan file htaccess beserta file2 lain

--- API/fetchdetailinfo.php
+++ API/fetchdetailinfo.php
@@ -15,15 +15,11 @@ function getDetail()
         global $koneksi;
         $id_warung = $_POST["id"];
         $query = mysqli_query($koneksi, 
-        "SELECT tb_warung.id_warung,tb_warung.nama_warung ,COUNT(tb_rating.rating) AS JUMLAH, tb_fotomenu.foto_menu, tb_deskripsi.deskripsi
+        "SELECT tb_warung.id_warung,tb_warung.nama_warung ,COUNT(tb_rating.rating) AS JUMLAH
         FROM tb_warung
         LEFT OUTER JOIN tb_rating
-        ON tb_warung.id_warung = tb_rating.id_warung AND tb_warung.id_warung
-        LEFT OUTER JOIN tb_fotomenu
-        ON tb_warung.id_warung = tb_fotomenu.id_warung
-        JOIN tb_deskripsi
-        ON tb_fotomenu.id_deskrpsi = tb_deskripsi.id_deskrpsi AND tb_deskripsi.id_warung = tb_warung.id_warung
-        WHERE tb_warung.id_warung=$id_warung");
+        ON tb_warung.id_warung = tb_rating.id_warung AND tb_warung.id_warung=$id_warung
+        ");
         if(mysqli_num_rows($query)){
             $row = mysqli_fetch_assoc($query);
             echo(json_encode($row));
--- API/home.php
+++ API/home.php
@@ -14,7 +14,7 @@ function getWarung()
 {
     global $koneksi;
     //$warung = mysqli_query($koneksi, "SELECT tb_warung.nama_warung, tb_warung.alamat, tb_rating.rating, tb_warung.foto FROM tb_warung JOIN tb_rating ON tb_warung.id_warung = tb_rating.id_warung");
-    $warung =mysqli_query($koneksi,"SELECT tb_warung.nama_warung, tb_warung.alamat, AVG(tb_rating.rating) AS rating, tb_warung.foto 
+    $warung =mysqli_query($koneksi,"SELECT tb_warung.id_warung,tb_warung.nama_warung, tb_warung.alamat, AVG(tb_rating.rating) AS rating, tb_warung.foto 
     FROM tb_warung 
     JOIN tb_rating 
     ON tb_warung.id_warung = tb_rating.id_warung
